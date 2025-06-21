40650907a6e354192b258182b2a85e8678a9aa14
CesarFloresGutierrez <chesaresuport@gmail.com>
2025-06-20 18:55:12 -0600

    creacion de cajero cliente

---
+++ src/main/java/com/digis01/MMarinCajeroClient/MMarinCajeroClientApplication.java
@@ -0,0 +1,13 @@
+package com.digis01.MMarinCajeroClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MMarinCajeroClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MMarinCajeroClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digis01/MMarinCajeroClient/MMarinCajeroClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digis01.MMarinCajeroClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MMarinCajeroClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
